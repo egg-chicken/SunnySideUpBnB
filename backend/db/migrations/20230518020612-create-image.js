d8d4d7f90b6afa93023e09504886ddc4f023dd1e
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-05-17 21:57:17 -0700

    migrations and models starter

---
+++ backend/db/migrations/20230518020612-create-image.js
@@ -0,0 +1,36 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      imageableId: {
+        type: Sequelize.INTEGER
+      },
+      imageableType: {
+        type: Sequelize.ENUM('User', 'Review', 'Spot')
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Images');
+  }
+};
---
+++ backend/db/models/image.js
@@ -0,0 +1,37 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Image.belongsTo(models.Review, {
+        foreignKey: 'imageableId',
+        constraints: false
+      });
+      Image.belongsTo(models.Spot, {
+        foreignKey: 'imageableId',
+        constraints: false
+      });
+      Image.belongsToOne(models.User, {
+        foreignKey: 'imageableId',
+        constraints: false
+      })
+    }
+  }
+  Image.init({
+    url: DataTypes.STRING,
+    imageableId: DataTypes.INTEGER,
+    imageableType: DataTypes.ENUM('User', 'Review', 'Spot')
+  }, {
+    sequelize,
+    modelName: 'Image',
+  });
+  return Image;
+};
