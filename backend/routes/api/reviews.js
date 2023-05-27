cb4e97449b946a5adf73eb2b0bcef9fe2439bd05
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-05-27 11:39:45 -0700

    get all reviews by a spot's id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcryptjs');
 const { Spot, Image, Booking, User, Review, sequelize } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
 
+//get all reviews of the current user - not complete
 router.get('/current', requireAuth, async (req, res) => {
     const currentUser = req.user;
     const reviews = await Review.findAll({
@@ -35,8 +36,36 @@ router.get('/current', requireAuth, async (req, res) => {
         ],
       });
     res.json({Reviews: reviews})
-})
+});
 
+//get all reviews by a spot's id
+router.get('/:id/reviews', async (req, res) => {
+    const { id } = req.params;
 
+    const spot = await Spot.findByPk(id);
+
+    if (!spot) {
+      return res.status(404).json({ message: "Spot couldn't be found" });
+    }
+
+    const reviews = await Review.findAll({
+        where: {
+          id
+        },
+        include: [
+          {
+            model: User,
+            attributes: ['id', 'firstName', 'lastName']
+          },
+          {
+            model: Image,
+            as: 'ReviewImages',
+            attributes: ['id', 'url']
+          }
+        ]
+      });
+
+      res.json({ Reviews: reviews });
+});
 
 module.exports = router;
