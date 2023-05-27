13e2995232f6889caa815828e3da4c3c918c09d4
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-05-26 22:59:48 -0700

    boolean preview edit

--- backend/db/seeders/20230523024249-images.js
+++ backend/db/seeders/20230523024249-images.js
@@ -24,7 +24,7 @@ module.exports = {
       {
         id: 1,
         url: 'image1 url',
-        preview: 'true',
+        preview: true,
         imageableId: 1,
         imageableType: 'Spot',
         createdAt: new Date(),
@@ -33,7 +33,7 @@ module.exports = {
       {
         id: 2,
         url: 'image2 url',
-        preview: 'true',
+        preview: true,
         imageableId: 2,
         imageableType: 'Spot',
         createdAt: new Date(),
@@ -42,7 +42,7 @@ module.exports = {
       {
         id: 3,
         url: 'image3 url',
-        preview: 'true',
+        preview: true,
         imageableId: 3,
         imageableType: 'Spot',
         createdAt: new Date(),
