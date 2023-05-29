52b23c90c74e779895368c29d83b0e4d2d28cf5e
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-05-28 18:53:18 -0700

    testing 2

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -82,7 +82,7 @@ router.get('/:id', async (req, res) => {
         attributes: {
             include: [
                 [
-                    sequelize.fn("COUNT", sequelize.cast(sequelize.col("Reviews.review"), 'INTEGER')),"numReviews"
+                    sequelize.fn("COUNT", sequelize.col("Reviews.id")),"numReviews", 'INTEGER'
                 ],
                 [
                     sequelize.fn("AVG", sequelize.cast(sequelize.col("Reviews.stars"), 'FLOAT')),"avgStarRating"
