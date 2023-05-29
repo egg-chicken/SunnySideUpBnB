225f8f2403f3585bb8c1690b679ad5f65ce661b2
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-05-28 17:44:08 -0700

    using parseint for numReviews

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -109,6 +109,8 @@ router.get('/:id', async (req, res) => {
         group: ['Spot.id', 'SpotImages.id', 'Owner.id']
     });
 
+    detailId.numReviews = parseInt(detailId.numReviews);
+    
     if (!detailId) {
         res.status(404).json({ message: "Spot couldn't be found" });
       } else {
