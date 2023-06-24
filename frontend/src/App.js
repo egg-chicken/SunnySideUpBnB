9b34ee811ffaefac2ca8dbd8326ffbdf9e1bceab
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-06-24 11:48:55 -0700

    Navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -13,8 +14,10 @@ function App() {
   }, [dispatch]);
 
   return (
-    isLoaded && (
-      <Switch>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
         <Route path="/login">
           <LoginFormPage />
         </Route>
@@ -22,7 +25,8 @@ function App() {
           <SignupFormPage />
         </Route>
       </Switch>
-    )
+      )}
+    </>
   );
 }
 
---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,13 @@
+
+function ProfileButton() {
+    return (
+        <>
+        <button>
+            <i className="fas fa-user-circle"></i>
+        </button>
+
+        </>
+    )
+}
+
+export default ProfileButton;
