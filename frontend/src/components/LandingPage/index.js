dedcaa3d1f8b80a2cc51d580f78e1f7dd12bc134
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-06-27 20:50:21 -0700

    updated Landing page css

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -3,6 +3,8 @@ import { useDispatch } from 'react-redux';
 import { getSpots } from '../../store/spots';
 import { useSelector } from 'react-redux';
 
+import './Landing.css'
+import { Link } from 'react-router-dom';
 const LandingPage = () => {
     const dispatch = useDispatch();
     const spots = useSelector((state) => state.spot);
@@ -14,14 +16,18 @@ const LandingPage = () => {
 
 
     return (
-        <main>
+        <main className='tiles-container'>
             {spots && spotsArray.map((spot) => (
-                <div key={spot.id}>
-                    <img src={spot.url} alt={spot.name} />
-                    <p>{`${spot.city}, ${spot.state}`}</p>
-                    <p>{spot.name}</p>
-                    <p>{spot.rating}</p>
-                    <p>{`${spot.price} night`}</p>
+                <div key={spot.id} className='spot-tile'>
+                    <Link to={`/spots/${spot.id}`}>test</Link>
+                    <img src={spot.url} alt={spot.name} className='image'/>
+                    <div className='spot-details'>
+                        <p className='spot-location'>{`${spot.city}, ${spot.state}`}</p>
+                        {/* <p className='spot-name'>{spot.name}</p> */}
+                        {/* <ReactToolTip role='tooltip' tip-position='center' >{spot.name}</ReactToolTip> */}
+                        <p className='spot-rating'>{spot.rating || 'New'}</p>
+                        <p className='spot-price'>{`$${spot.price} night`}</p>
+                    </div>
                 </div>
             ))}
         </main>
