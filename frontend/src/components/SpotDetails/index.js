6b4fef8933fcdb4fa7dd5d6133ca7d5a3be28abe
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-06-28 01:02:43 -0700

    spotDetails updated

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -2,6 +2,7 @@ import React, { useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOneSpot } from '../../store/spots';
 import { useParams } from 'react-router-dom';
+import './spotDetails.css'
 
 const SpotDetails = () => {
     const { id } = useParams();
@@ -13,20 +14,24 @@ const SpotDetails = () => {
         dispatch(getOneSpot(id));
     }, [id, dispatch]);
 
-    // return (
-    //     <>
-    //         <div className='spot-details'>
-    //             {/* <h1>{spotId.name}</h1> */}
-    //             test
-    //         </div>
-    //     </>
-    // )
+    const handleClick = e => {
+        e.preventDefault();
+        alert("Feature Coming Soon...")
+    }
+
     return (
         <div className='spot-details'>
           {spot ? (
             <>
               <h1>{spot.name}</h1>
-              {/* Render other spot details */}
+              <p>{spot.city}, {spot.state}, {spot.country}</p>
+              <img src={spot.url} alt={spot.name} className='image-detail-spot'/>
+              <p>Hosted by {spot.firstName} {spot.lastName}</p>
+              <p>{spot.description}</p>
+              <div className='callout-box'>
+                <p>Price: ${spot.price} night</p>
+                <button onClick={handleClick} className='reserve-button'>Reserve</button>
+              </div>
             </>
           ) : (
             <p>Loading spot details...</p>
