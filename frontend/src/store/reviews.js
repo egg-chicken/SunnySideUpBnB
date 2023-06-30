556c31499580dd5f627098d7ac7c399623e8c5b6
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-06-29 21:07:06 -0700

    Reviews action + reducer

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,5 +1,35 @@
 import { csrfFetch } from "./csrf";
 
-const LOAD = 'spots/LOAD';
+const LOAD = 'reviews/LOAD';
 
+const load = reviews => ({
+    type: LOAD,
+    reviews
+});
 
+//get all the reviews
+export const getAllReviews = id => async dispatch => {
+    const response = await csrfFetch(`/api/spots/${id}/reviews`)
+
+    if(response.ok){
+        const reviews = await response.json();
+        dispatch(load(reviews));
+        return reviews;
+    }
+}
+
+const initialState = {}
+const reviewsReducer = (state = initialState, action) => {
+    let newState = {...state};
+    switch (action.type) {
+        case LOAD:
+            action.reviews.forEach(review => {
+                newState[review.id] = review;
+            });
+            return newState;
+        default:
+            return newState;
+    }
+}
+
+export default reviewsReducer;
