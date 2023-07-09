21052e1380af958c10ebbdbfe41b4e7490a0aec8
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-07-09 01:01:57 -0700

    fixed spot details page

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -1,19 +1,24 @@
-import React, { useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getOneSpot } from '../../store/spots';
-// import { getAllReviews } from '../../store/reviews';
+import * as spotsActions from '../../store/spots';
+import * as reviewsActions from '../../store/reviews';
 import { useParams } from 'react-router-dom';
 import './spotDetails.css'
 
 const SpotDetails = () => {
     const { id } = useParams();
     const dispatch = useDispatch();
-    const spot = useSelector((state) => state.spot[id])
-    // const reviews = useSelector(state => Object.values(state.review))
+    const spot = useSelector((state) => state.spot[id]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [isReviewsLoaded, setIsReviewsLoaded] = useState(false);
+    const reviews = useSelector((state) => Object.values(state.reviews.spotReviews));
 
     useEffect(() => {
         // console.log('!!!!spots!!!!', id)
-        dispatch(getOneSpot(id));
+        dispatch(spotsActions.getOneSpot(id))
+          .then(() => setIsLoaded(true))
+        dispatch(reviewsActions.getAllReviews(id))
+          .then(() => setIsReviewsLoaded(true))
         // dispatch(getAllReviews(id))
     }, [id, dispatch]);
 
@@ -23,47 +28,71 @@ const SpotDetails = () => {
     }
 
     return (
-        <div className='parent'>
-          {spot ? (
-            <>
-              <div className='spot-details'>
-                <h1>{spot.name}</h1>
-                <p className='location'>{spot.city}, {spot.state}, {spot.country}</p>
-              </div>
-              <div className='div2'>
-                <img src={spot.SpotImages[0].url} alt={spot.name} className='image-detail-spot'/>
-              </div>
-              <div className='div3'>
-                <img src={spot.url} alt={spot.name} className='image-detail-spot'/>
-              </div>
-              <div className='div4'>
-                <img src={spot.url} alt={spot.name} className='image-detail-spot'/>
-              </div>
-              <div className='div5'>
-                <img src={spot.url} alt={spot.name} className='image-detail-spot'/>
-              </div>
-              <div className='div6'>
-                <img src={spot.url} alt={spot.name} className='image-detail-spot'/>
-              </div>
-              <div className='div8'>
-                <p>Hosted by {spot.firstName} {spot.lastName}</p>
-                <p>{spot.description}</p>
-              </div>
-              <div className='div7'>
-                {/* <i className="fa-solid fa-star"/> */}
-                <p>Reviews . . .</p>
-              </div>
-              <div className='callout-box'>
-                <p>Price: ${spot.price} night</p>
-                {/* <i className="fa-solid fa-star"/> */}
-                <button onClick={handleClick} className='reserve-button'>Reserve</button>
-              </div>
-            </>
-          ) : (
-            <p>Loading spot details...</p>
-          )}
-        </div>
+        // <div className='parent'>
+        //   {spot ? (
+        //     <>
+        //       <div className='spot-details'>
+        //         <h1>{spot.name}</h1>
+        //         <p className='location'>{spot.city}, {spot.state}, {spot.country}</p>
+        //       </div>
+        //       <div className='div2'>
+        //         <img src={spot.SpotImages[0].url} alt={spot.name} className='image-detail-spot'/>
+        //       </div>
+        //       <div className='div8'>
+        //         <p>Hosted by {spot.firstName} {spot.lastName}</p>
+        //         <p>{spot.description}</p>
+        //       </div>
+        //       <div className='div7'>
+        //         {/* <i className="fa-solid fa-star"/> */}
+        //         <p>Reviews . . .</p>
+        //       </div>
+        //       <div className='callout-box'>
+        //         <p>Price: ${spot.price} night</p>
+        //         {/* <i className="fa-solid fa-star"/> */}
+        //         <button onClick={handleClick} className='reserve-button'>Reserve</button>
+        //       </div>
+        //     </>
+        //   ) : (
+        //     <p>Loading spot details...</p>
+        //   )}
+        // </div>
+      <>
+        {isLoaded && (
+          <div>
+            <div className='spot-details'>
+                 <h1>{spot.name}</h1>
+                 <p className='location'>{spot.city}, {spot.state}, {spot.country}</p>
+           </div>
+           <div className='spot-desc'>
+                 <p>Hosted by {spot.firstName} {spot.lastName}</p>
+                 <p>{spot.description}</p>
+           </div>
+           <div className='spot-images'>
+              {
+                spot?.SpotImages.map((img, index) => {
+                  return (
+                    <img className={img.preview ? 'spot-preview-img' : `spot-alt-img alt-img-${index}`} src={img.url} onError={(e) => {
+                      e.target.src = 'https://res.cloudinary.com/dc5lrkblw/image/upload/v1688368793/airbnb-proj/No-Image-Placeholder_wthyue.svg';
+                      e.onError = null;
+                    }} alt={img.url.split('/').pop()} />
+                  );
+                })
+              }
+           </div>
+           <div className='callout-box'>
+               <p>Price: ${spot.price} night</p>
+               {/* <i className="fa-solid fa-star"/> */}
+               <button onClick={handleClick} className='reserve-button'>Reserve</button>
+           </div>
 
+          </div>
+        )}
+        {isReviewsLoaded &&
+
+      
+
+        }
+      </>
       );
 }
 
