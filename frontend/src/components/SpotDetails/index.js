84c6a7dd5250c4104fa05c8448786337187e5311
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-07-09 15:46:59 -0700

    cleaned up spotdetails page

--- frontend/src/components/SpotDetails/index.js
+++ frontend/src/components/SpotDetails/index.js
@@ -28,84 +28,56 @@ const SpotDetails = () => {
     }
 
     return (
-        // <div className='parent'>
-        //   {spot ? (
-        //     <>
-        //       <div className='spot-details'>
-        //         <h1>{spot.name}</h1>
-        //         <p className='location'>{spot.city}, {spot.state}, {spot.country}</p>
-        //       </div>
-        //       <div className='div2'>
-        //         <img src={spot.SpotImages[0].url} alt={spot.name} className='image-detail-spot'/>
-        //       </div>
-        //       <div className='div8'>
-        //         <p>Hosted by {spot.firstName} {spot.lastName}</p>
-        //         <p>{spot.description}</p>
-        //       </div>
-        //       <div className='div7'>
-        //         {/* <i className="fa-solid fa-star"/> */}
-        //         <p>Reviews . . .</p>
-        //       </div>
-        //       <div className='callout-box'>
-        //         <p>Price: ${spot.price} night</p>
-        //         {/* <i className="fa-solid fa-star"/> */}
-        //         <button onClick={handleClick} className='reserve-button'>Reserve</button>
-        //       </div>
-        //     </>
-        //   ) : (
-        //     <p>Loading spot details...</p>
-        //   )}
-        // </div>
       <>
         <div className='full-page'>
-        {isLoaded && (
-          <div key={spot.id}>
-            <div className='spot-details'>
-                 <h1>{spot.name}</h1>
-                 <p className='location'>{spot.city}, {spot.state}, {spot.country}</p>
-           </div>
-           <div className='spot-desc'>
-                 <p>Hosted by {spot.firstName} {spot.lastName}</p>
-                 <p>{spot.description}</p>
-           </div>
-           <div className='spot-images'>
-              {
-                spot?.SpotImages.map((img, index) => {
-                  return (
-                    <img className={img.preview ? 'spot-preview-img' : `spot-alt-img alt-img-${index}`} src={img.url} onError={(e) => {
-                      e.target.src = 'https://res.cloudinary.com/dc5lrkblw/image/upload/v1688368793/airbnb-proj/No-Image-Placeholder_wthyue.svg';
-                      e.onError = null;
-                    }} alt={img.url.split('/').pop()} />
-                  );
-                })
+          {isLoaded && (
+            <div key={spot.id}>
+              <div className='spot-details'>
+                  <h1>{spot.name}</h1>
+                  <p className='location'>{spot.city}, {spot.state}, {spot.country}</p>
+            </div>
+            <div className='spot-desc'>
+                  <p>Hosted by {spot.firstName} {spot.lastName}</p>
+                  <p>{spot.description}</p>
+            </div>
+            <div className='spot-images'>
+                {
+                  spot?.SpotImages.map((img, index) => {
+                    return (
+                      <img className={img.preview ? 'spot-preview-img' : `spot-alt-img alt-img-${index}`} src={img.url} onError={(e) => {
+                        e.target.src = 'https://res.cloudinary.com/dc5lrkblw/image/upload/v1688368793/airbnb-proj/No-Image-Placeholder_wthyue.svg';
+                        e.onError = null;
+                      }} alt={img.url.split('/').pop()} />
+                    );
+                  })
+                }
+            </div>
+            <div className='callout-box'>
+                <p>${spot.price} night</p>
+                {/* <i className="fa-solid fa-star"/> */}
+                <button onClick={handleClick} className='reserve-button'>Reserve</button>
+            </div>
+
+            </div>
+          )}
+          {isReviewsLoaded &&
+            <div>
+              <h2><i className="fa-solid fa-star"/>{spot.avgStarRating || 'New'}</h2>
+              {reviews?.map(review => {
+                return (
+                  <div>
+                    <p>{review.User?.firstName}</p>
+                    <p>{review.createdAt}</p>
+                    <p>{review.review}</p>
+                  </div>
+                )
+              })
+
               }
-           </div>
-           <div className='callout-box'>
-               <p>${spot.price} night</p>
-               {/* <i className="fa-solid fa-star"/> */}
-               <button onClick={handleClick} className='reserve-button'>Reserve</button>
-           </div>
-
-          </div>
-        )}
-        {isReviewsLoaded &&
-          <div>
-            <h2><i className="fa-solid fa-star"/>{spot.avgStarRating || 'New'}</h2>
-            {reviews?.map(review => {
-              return (
-                <div>
-                  <p>{review.User?.firstName}</p>
-                  <p>{review.createdAt}</p>
-                  <p>{review.review}</p>
-                </div>
-              )
-            })
-
-            }
-          </div>
+            </div>
 
 
-        }
+          }
         </div>
       </>
       );
