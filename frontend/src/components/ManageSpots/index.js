bbe6645cd38334baba2d5bc70973294ca263e786
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-07-11 21:57:09 -0700

    updated manage spots component

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import * as spotsActions from '../../store/spots';
 import { useDispatch, useSelector } from 'react-redux';
 
 const ManageSpots = () => {
     const dispatch = useDispatch();
+    const history = useHistory();
     const spots = useSelector((state) => state.spot);
     const spotsArray = Object.values(spots);
 
@@ -14,6 +16,7 @@ const ManageSpots = () => {
     return (
         <>
             <h2>Manage Your Spots</h2>
+            <button onClick={() => history.push('/spots/new')}>Create a New Spot</button>
             <div>
                 {spots && spotsArray.map((spot) => {
                     <div>
