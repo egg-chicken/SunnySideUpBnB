9ce66135588a0ed243dbf041d2fc06a3d4fd6515
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2023-07-12 01:01:07 -0700

    idk if it works

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import * as spotsActions from '../../store/spots';
 import { useDispatch, useSelector } from 'react-redux';
+// import SpotGrid from '../SpotGrid';
+import { Link } from 'react-router-dom';
+
 
 const ManageSpots = () => {
     const dispatch = useDispatch();
@@ -13,17 +16,32 @@ const ManageSpots = () => {
         dispatch(spotsActions.getCurrentSpots())
     }, [dispatch]);
 
+    const defaultImage = 'https://res.cloudinary.com/dc5lrkblw/image/upload/v1688368793/airbnb-proj/No-Image-Placeholder_wthyue.svg'
     return (
         <>
             <h2>Manage Your Spots</h2>
-            <button onClick={() => history.push('/spots/new')}>Create a New Spot</button>
-            <div>
-                {spots && spotsArray.map((spot) => {
-                    <div>
-
+            <main className='tiles-container'>
+            {spots && spotsArray.map((spot) => (
+                <div key={spot.id} className='spot-tile'>
+                    <div  className='tooltip'>
+                        <Link className='spot-id' to={`/spots/${spot.id}`}>
+                            <p className='tooltiptext'>{spot.name}</p>
+                            <img src={spot.previewImage || defaultImage} alt='Spot Preview' className='image'title={spot.name}/>
+                            <div className='spot-details'>
+                                <div className='local-rating'>
+                                    <p className='spot-location'>{`${spot.city}, ${spot.state}`}</p>
+                                    <p className='spot-rating'>
+                                        <i className="fa-solid fa-star"/>
+                                        {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(2)) : 'New'}
+                                    </p>
+                                </div>
+                                <p className='spot-price'>{`$${spot.price} night`}</p>
+                            </div>
+                        </Link>
                     </div>
-                })}
-            </div>
+                </div>
+            ))}
+        </main>
         </>
     )
 };
