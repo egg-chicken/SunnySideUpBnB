6e9e6ea239dca96e5aca4f9872b33178e784f2ee
Edith Gomez Garcia <ggarcia.edith@gmail.com>
2024-02-10 21:04:32 -0800

    fixed the landing page responsiveness

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -20,28 +20,23 @@ const LandingPage = () => {
     return (
         <main >
             <div className='tiles-container'>
-            {spots && spotsArray.map((spot) => (
-                <div key={spot.id} className='spot-tile'>
-                    <div  className='tooltip'>
-                        <Link className='spot-id' to={`/spots/${spot.id}`}>
-                            <p className='tooltiptext'>{spot.name}</p>
-                            <img src={spot.previewImage || defaultImage} alt='Spot Preview' className='image'title={spot.name} onError={(e) => {
-                        e.target.src = 'https://res.cloudinary.com/dc5lrkblw/image/upload/v1688368793/airbnb-proj/No-Image-Placeholder_wthyue.svg';
-                        e.onError = null;}}/>
-                            <div className='spot-details'>
-                                <div className='local-rating'>
-                                    <p className='spot-location'>{`${spot.city}, ${spot.state}`}</p>
-                                    <p className='spot-rating'>
-                                        <i className="fa-solid fa-star"/>
-                                        {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(2)) : 'New'}
-                                    </p>
-                                </div>
-                                <p className='spot-price'><span className='price-child'>{`$${spot.price}`}</span> night</p>
+                {spotsArray.map((spot) => (
+                    <Link key={spot.id} className='spot-id spot-tile' to={`spots/${spot.id}`}>
+                        <img src={spot.previewImage || defaultImage} alt='preview' className='image' title={spot.name} onError={(e) => {
+                            e.target.src = 'https://res.cloudinary.com/dc5lrkblw/image/upload/v1688368793/airbnb-proj/No-Image-Placeholder_wthyue.svg';
+                            e.onError = null;}}/>
+                        <div className='spot-details'>
+                            <div className='local-rating'>
+                                <p className='spot-location'>{`${spot.city}, ${spot.state}`}</p>
+                                <p className='spot-rating'>
+                                    <i className="fa-solid fa-star"/>
+                                    {spot.avgRating ? (Number.isInteger(spot.avgRating) ? spot.avgRating.toFixed(1) : spot.avgRating.toFixed(2)) : 'New'}
+                                </p>
                             </div>
-                        </Link>
-                    </div>
-                </div>
-            ))}
+                            <p className='spot-price'><span className='price-child'>{`$${spot.price}`}</span> night</p>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </main>
     )
